5493f6a154204be9cb61a6dd586fefe23593e2a8
fanyong920 <45630410+fanyong920@users.noreply.github.com>
2020-01-22 15:57:56 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,31 +1,17 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.storage.sync.set({color: '#3aa757'}, function() {
-//     console.log('The color is green.');
-//   });
-//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//     chrome.declarativeContent.onPageChanged.addRules([{
-//       conditions: [new chrome.declarativeContent.PageStateMatcher({
-//         pageUrl: {hostEquals: 'http://*.*.*/*'},
-//       })
-//       ],
-//           actions: [new chrome.declarativeContent.ShowPageAction()]
-//     }]);
-//   });
 
-  chrome.runtime.onMessage.addListener(function(message ,send,response){
-    console.log(message)
-    $.ajax({
-      url: "http://localhost:8089/getUrl",
-      type: "POST",
-      data: message,
-      crossDomain: true, 
-      xhrFields: {          //携带cookie
-        withCredentials: false
-    },
-      contentType:"application/x-www-form-urlencoded; charset=UTF-8",
-      complete:function(xhr,status){
-        response({"isDone":true})
-      }
-    })
-  })
-// });
+chrome.runtime.onMessage.addListener(function(message,send,response){
+var param ={
+	"content":message
+}
+$.ajax({
+  url: "http://localhost:8089/getUrl",
+  type: "POST",
+  data: message,
+  contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+  complete:function(XMLHttpRequest, textStatus){
+	  var tab = new Array(1);
+	  tab[0] = send.tab.id;
+	  chrome.tabs.remove(tab);
+  }
+})
+})
